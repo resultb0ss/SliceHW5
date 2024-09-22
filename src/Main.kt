4471dd146fae98eb422bc77625371c933ebf0098
Admin <reteamboss@gmail.com>
2024-09-22 23:25:44 +0300

    5

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,12 @@
 fun main() {
-    println("Hello World!")
+    val array: List<Product> = arrayListOf(
+        Product("Хлеб",23),
+        Product("Картошка",34),
+        Product("Колбаса",2),
+        Product("Арбуз",34),
+        Product("Помидоры",53),
+    )
+
+    val count = array.fold(0){sum,elem -> sum + elem.count}
+    println(count)
 }
--- src/Product.kt
+++ src/Product.kt
@@ -1,2 +1,3 @@
-class Product {
+class Product (val name: String, val count: Int) {
+
 }
